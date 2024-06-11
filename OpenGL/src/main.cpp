8b761da5a123668f75ff9004e0cc3446a973e842
xieguo <xieguo@bytedance.com>
2024-06-11 21:57:43 +0800

    38 glDrawArray.
    Signed-off-by: xieguo <xieguo@bytedance.com>

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "application/application.h">
+#include "application/application.h"
 #include "wrapper/checkError.h"
 
 using namespace std;
+
+GLuint program = 0, vao = 0;
 
 // declear a function to respond window resize
 void onResize(int width, int height)
@@ -33,70 +35,6 @@ void onKey(int key, int action, int modes)
     }
 }
 
-void prepareVBO() {
-    GLuint vbo = 0;
-    GL_CALL(glGenBuffers(1, &vbo));
-    GL_CALL(glDeleteBuffers(1, &vbo));
-
-    GLuint vboArr[] = {0, 0, 0};
-    GL_CALL(glGenBuffers(3, vboArr));
-    GL_CALL(glDeleteBuffers(3, vboArr));
-}
-
-void prepare() {
-    float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.0f, 0.5f, 0.0f
-    };
-
-    GLuint vbo = 0;
-    GL_CALL(glGenBuffers(1, &vbo));
-    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, vbo));
-    GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
-}
-
-void prepareSingleBuffer() {
-    float positions[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.0f, 0.5f, 0.0f
-    };
-
-    float colors[] = {
-            1.0f, 0.0f, 0.0f,
-            0.0f, 1.0f, 0.0f,
-            0.0f, 0.0f, 1.0f
-    };
-
-    GLuint posVbo = 0;
-    GL_CALL(glGenBuffers(1, &posVbo));
-    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, posVbo));
-    GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW));
-
-    GLuint colorVbo = 0;
-    GL_CALL(glGenBuffers(1, &colorVbo));
-    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, colorVbo));
-    GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW));
-
-    GLuint vao = 0;
-    GL_CALL(glGenVertexArrays(1, &vao));
-    GL_CALL(glBindVertexArray(vao));
-
-    // pos bind to vao
-    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, posVbo));
-    GL_CALL(glEnableVertexAttribArray(0));
-    GL_CALL(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0));
-
-    // color bind to vao
-    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, posVbo));
-    GL_CALL(glEnableVertexAttribArray(1));
-    GL_CALL(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0));
-
-    // unbind vao
-    GL_CALL(glBindVertexArray(0));
-}
-
 void prepareInterleavedBuffer() {
     float vertices[] = {
             -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
@@ -109,7 +47,6 @@ void prepareInterleavedBuffer() {
     GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, vbo));
     GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
 
-    GLuint vao = 0;
     GL_CALL(glGenVertexArrays(1, &vao));
     GL_CALL(glBindVertexArray(vao));
 
@@ -118,7 +55,7 @@ void prepareInterleavedBuffer() {
     GL_CALL(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0));
 
     GL_CALL(glEnableVertexAttribArray(1));
-    GL_CALL(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float))));
+    GL_CALL(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float))));
 
     // unbind vao
     GL_CALL(glBindVertexArray(0));
@@ -134,7 +71,7 @@ void prepareShader(){
             "}\0";
 
     const char* fragmentShaderSource =
-            "#version 330 core\n"
+            "#version 410 core\n"
             "out vec4 FragColor;\n"
             "void main()\n"
             "{\n"
@@ -143,49 +80,63 @@ void prepareShader(){
 
     // create shader program (vs、fs)
     GLuint vertex, fragment;
+
     vertex = glCreateShader(GL_VERTEX_SHADER);
     fragment = glCreateShader(GL_FRAGMENT_SHADER);
 
     // input shader code
-    glShaderSource(vertex, 1, &vertexShaderSource, NULL);
-    glShaderSource(fragment, 1, &fragmentShaderSource, NULL);
+    GL_CALL(glShaderSource(vertex, 1, &vertexShaderSource, NULL));
+    GL_CALL(glShaderSource(fragment, 1, &fragmentShaderSource, NULL));
 
     int success = 0;
     char infoLog[1024];
     // compile shader code
-    glCompileShader(vertex);
-    glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+    GL_CALL(glCompileShader(vertex));
+    GL_CALL(glGetShaderiv(vertex, GL_COMPILE_STATUS, &success));
     if (!success) {
-        glGetShaderInfoLog(vertex, 1024, NULL, infoLog);
+        GL_CALL(glGetShaderInfoLog(vertex, 1024, NULL, infoLog));
         cout << "Error: SHADER COMPILE ERROR --VERTEX" << "\n" << infoLog << endl;
     }
 
-    glCompileShader(fragment);
-    glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+    GL_CALL(glCompileShader(fragment));
+    GL_CALL(glGetShaderiv(fragment, GL_COMPILE_STATUS, &success));
     if (!success) {
-        glGetShaderInfoLog(fragment, 1024, NULL, infoLog);
+        GL_CALL(glGetShaderInfoLog(fragment, 1024, NULL, infoLog));
         cout << "Error: SHADER COMPILE ERROR --FRAGMENT" << "\n" << infoLog << endl;
     }
 
     // create a program
-    GLuint program = 0;
     program = glCreateProgram();
 
     // put the compiled result of vs & fs to program
-    glAttachShader(program, vertex);
-    glAttachShader(program, fragment);
+    GL_CALL(glAttachShader(program, vertex));
+    GL_CALL(glAttachShader(program, fragment));
 
     // execute program, and create the executable shader
-    glLinkProgram(program);
-    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    GL_CALL(glLinkProgram(program));
+    GL_CALL(glGetProgramiv(program, GL_LINK_STATUS, &success));
     if (!success) {
-        glGetShaderInfoLog(program, 1024, NULL, infoLog);
+        GL_CALL(glGetProgramInfoLog(program, 1024, NULL, infoLog));
         cout << "Error: SHADER LINK ERROR " << "\n" << infoLog << endl;
     }
 
     // clean up
-    glDeleteShader(vertex);
-    glDeleteShader(fragment);
+    GL_CALL(glDeleteShader(vertex));
+    GL_CALL(glDeleteShader(fragment));
+}
+
+void render() {
+    // clear buffer
+    GL_CALL(glClear(GL_COLOR_BUFFER_BIT));
+
+    // 1. bind current program
+    GL_CALL(glUseProgram(program));
+
+    // 2. bind current vao
+    GL_CALL(glBindVertexArray(vao));
+
+    // 3. send draw call
+    GL_CALL(glDrawArrays(GL_TRIANGLES, 0, 3));
 }
 
 int main(void)
@@ -198,7 +149,7 @@ int main(void)
 
     // Set openGL view port and clear color
     GL_CALL(glViewport(0, 0, 800, 600));
-    GL_CALL(glClearColor(0.2f, 0.3f, 0.3f, 10.f));
+    GL_CALL(glClearColor(0.2f, 0.3f, 0.3f, 1.0f));
 
     prepareShader();
     prepareInterleavedBuffer();
@@ -206,11 +157,8 @@ int main(void)
     /* Loop until the user closes the window */
     while (app->update())
     {
-        // clear buffer
-        GL_CALL(glClear(GL_COLOR_BUFFER_BIT));
-
         // render
-
+        render();
     }
 
     app->destroy();
--- wrapper/checkError.h
+++ wrapper/checkError.h
@@ -1,7 +1,14 @@
 #pragma once
 
 #ifdef DEBUG
-#define GL_CALL(func) func;checkError();
+#define GL_CALL(x) do { \
+    x; \
+    GLenum error = glGetError(); \
+    if (error != GL_NO_ERROR) { \
+        std::cerr << "OpenGL Error: " << error << " at line " << __LINE__ << std::endl; \
+        exit(1); \
+    } \
+} while (0)
 #else
 #define GL_CALL(func) func;
 #endif
